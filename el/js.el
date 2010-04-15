a3615bcbcdab774edce858d53064d7e7fa226239
aka <akapav@gmail.com>
2010-04-15 15:03:33 +0200

    js.el: js-eval-region added

--- el/js.el
+++ el/js.el
@@ -2,6 +2,10 @@
   (interactive
    (let ((file (expand-file-name (read-file-name "File: " nil nil t))))
      `(,file)))
-  (slime-eval-async
-      `(js::js-load-file ,file)
-    (lambda (ret) (message (format "js: %s" ret)))))
+  (slime-eval-with-transcript
+   `(js::js-load-file ,file)))
+
+(defun js-eval-region (start end)
+  (interactive "r")
+  (slime-eval-with-transcript
+   `(js::!eval ,(buffer-substring-no-properties start end))))
